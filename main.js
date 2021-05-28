37483fa21791915dde925cb3111a8633c49bccac
Suryaprakash1024 <suryaprakash1024999@gmail.com>
2021-05-28 21:39:35 +0530

    Add filed via upload

---
+++ main.js
@@ -0,0 +1,50 @@
+const TelegramBot = require('node-telegram-bot-api');
+const https = require('https');
+const request = require('request');
+
+
+const token = '1876216085:AAErhJV1_cwtX2vic16baoHvWOZQZ7n2e3w';
+const bot = new TelegramBot(token, { polling: true });
+
+bot.on('message', msg => {
+  //anything
+  var hi = "hi";
+if (msg.text.toString().toLowerCase().indexOf(hi) === 0) {
+bot.sendMessage(msg.chat.id,"Hello dear user");
+} 
+    
+var bye = "bye";
+if (msg.text.toString().toLowerCase().includes(bye)) {
+bot.sendMessage(msg.chat.id, "Hope to see you around again , Bye");
+} 
+else
+    {
+//linkscrapper
+   // var password = "up"
+//if(msg.text.toString().toLowerCase().indexOf(password) === 0){
+   // bot.sendMessage(msg.chat.id,"Send Me DDL");
+    //bot.on('message', msg1 => {
+        link = msg.text.toString();
+        link = link.substring(22,link.dlength);
+        //bot.sendMessage(msg.chat.id,link);
+        const ModLink=link.split('/');
+        var movieTitle = ModLink[4].substring(0,ModLink[4].length - 4);
+        //bot.sendMessage(msg.chat.id,link);
+        const url = 'http://pdisk.net/api/ndisk_manager/video/create?link_type=link&content_src='+link+'&source=2000&uid=51081852&title='+movieTitle+'&description=Follow_@moviesnew24_for_more_movies';
+        //bot.sendMessage(msg.chat.id,url);
+        
+        //Web Req--
+        request(url, { json: true }, (err, res, body) => 
+                {
+                  if (err) { return console.log(err); }
+                    var id = body.data.item_id;
+            bot.sendMessage(msg.chat.id,'https://pdisk.net/share-video?videoid='+id);
+                  //console.log(body.explanation);
+                });
+        console.log('https://pdisk.net/share-video?videoid='+id);
+        bot.sendMessage(msg.chat.id,'https://pdisk.net/share-video?videoid='+id);
+        
+      // bot.sendMessage(msg.chat.id,movieTitle + " is Uploading..."); 
+
+}
+});
